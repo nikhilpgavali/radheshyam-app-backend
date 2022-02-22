61dd6b0e4b47a716be48c1c605490226620a58ed
nikjenni <gavalinikhil700@gmail.com>
2022-02-22 10:25:11 +0530

    added db file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { DbDailyActivity } from './db.daily-activity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(
       {
         type: "postgres",
-        host: 'postgres',
+        host: process.env.host || 'ec2-54-158-26-89.compute-1.amazonaws.com',
         port: 5432,
-        username: 'postgres' ,
-        password: 'postgres_password' ,
-        database: 'nik',
+        username: process.env.username || 'lhrgparcipqcci',
+        password: process.env.password || '27d619686584051a95e4a3400a8bdcd41eaf39124431d8039a59284f98201120',
+        database: process.env.db || 'dbn9pt7tmklkh',
         entities: [User],
         synchronize: false,
+        ssl: {
+          rejectUnauthorized:false
+        }
       }
-    ) 
+    )
   ],
-  controllers: [AppController],
+  controllers: [AppController, DbDailyActivity],
   providers: [AppService],
 })
 export class AppModule {
---
+++ src/db.daily-activity.ts
@@ -0,0 +1,38 @@
+import { Controller, Get } from "@nestjs/common";
+
+@Controller()
+export class DbDailyActivity {
+constructor(){}
+
+@Get('/setenvs')
+setEnvVariables(){
+    console.log("Heroku current envs:", process.env.DATABASE_URL);
+    console.log("username=>", process.env.username);
+    console.log("password=>", process.env.password);
+    console.log("hostname=>", process.env.host);
+    console.log("database=>", process.env.database);
+    let url = this.parseDatabaseUrl(process.env.DATABASE_URL);
+    if(process.env.username != url.username) {
+        process.env.username = url.username;
+    }
+    if(process.env.password != url.password) {
+        process.env.password = url.password;
+    }
+    if(process.env.host != url.host) {
+        process.env.host = url.host;
+    }
+    if(process.env.database != url.database) {
+        process.env.database = url.database;
+    }
+    return "Success";
+}
+
+parseDatabaseUrl(databaseUrl: string) {
+var url = databaseUrl.split(":");
+var userName = url[1].substring(2);
+var password = url[2].split("@")[0];
+var host = url[2].split("@")[1];
+var database = url[3].split('/')[1];
+return {username:userName, password: password,host:host,database:database};
+}
+}
